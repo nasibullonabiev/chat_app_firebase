a3e82483facce21ecc0b8c998307171e346efd4e
Nasibullo Nabiev <105308913+nasibullonabiev@users.noreply.github.com>
2025-05-23 17:22:35 +0200

    Connected with Firebase auth using CLI

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:chat_app_firebase/screens/auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:chat_app_firebase/firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
 
